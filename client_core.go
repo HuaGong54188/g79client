f54994ae85e59f9b9b5fa627d19be680d95d48bb
猫七街 <523528830@qq.com>
2025-09-30 17:24:10 +0800

    在client_core.go中将EngineVersion改为变量，添加Refetch函数以定期更新服务器信息，并在初始化时调用该函数，提升代码的动态性和实时性。

--- client_core.go
+++ client_core.go
@@ -3,9 +3,31 @@ package g79client
 import (
 	"net/http"
 	"strconv"
+	"time"
 )
 
-const EngineVersion = "3.5.5.278500"
+var EngineVersion = "3.5.5.278500"
+
+func Refetch() {
+	_, _ = RefreshTransferServers()
+	_, _ = RefreshLinkServers()
+	_, _ = RefreshChatServers()
+	packList, _ := RefreshPackList()
+	neteasePack, ok := packList["netease"]
+	if ok {
+		EngineVersion = neteasePack.Version
+	}
+}
+
+func init() {
+	Refetch()
+	go func() {
+		for {
+			time.Sleep(time.Second * 60)
+			Refetch()
+		}
+	}()
+}
 
 // Client 结构体
 type Client struct {
